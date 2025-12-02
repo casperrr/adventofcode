7c6341c672dd8c6dec58e19955fc67681b96b552
casperrr <casper.sebastien@gmail.com>
2025-12-02 00:06:02 +0000

    solved the first part

--- 2025/python/day1.py
+++ 2025/python/day1.py
@@ -1,2 +1,34 @@
+import dotenv
+import os
+import requests
 
 # Read in input
+dotenv.load_dotenv()
+aoc_session = os.getenv('AOC_SESSION')
+
+response = requests.get(
+    'https://adventofcode.com/2025/day/1/input',
+    cookies={'session': aoc_session}
+)
+# print(response.text)
+
+input_data = response.text.strip().split('\n')
+
+def parse(data):
+    direction, value = data[0], int(data[1:])
+    return -int(value) if direction == 'L' else int(value)
+
+turns = [parse(line) for line in input_data]
+
+# print(turns)
+
+def turn(pos, turn):
+    return (pos + turn) % 100
+
+positions = [50]
+for i, t in enumerate(turns):
+    positions.append(turn(positions[-1], t))
+
+part1 = len(list(filter(lambda x: x == 0, positions)))
+
+print(part1)
