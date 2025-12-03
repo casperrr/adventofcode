5de5481d1a60884c1f740d4ae1efaabf26c2ad6f
casperrr <casper.sebastien@gmail.com>
2025-12-03 20:25:20 +0000

    cleaned up code a lil

--- 2025/haskell/Day3.hs
+++ 2025/haskell/Day3.hs
@@ -1,7 +1,6 @@
 module Day3 where
 
 import Util.FetchInput ( fetchBodyStr, inputAOCURL )
-import Data.List
 
 type Bat = Int
 type Bank = [Bat]
@@ -17,10 +16,7 @@ parse :: String -> [Bank]
 parse = (map . map) (read . pure) . lines
 
 largest :: Bank -> Jolts
-largest bs = read $ show l1 ++ show l2
-    where
-        l1 = maximum $ init bs
-        l2 = (maximum . tail) $ dropWhile (/= l1) bs
+largest = largest' 2
 
 solve1 :: [Bank] -> Jolts
 solve1 = sum . map largest
@@ -35,35 +31,23 @@ takeSec n as = take (length as - n + 1) as
 output :: [Bat] -> Jolts
 output = read . concatMap show
 
------------------------------------------------------------------------------
--- Idk which one of these is cleanest so im in hell choosing so have them all
------------------------------------------------------------------------------
-large1 :: Int -> Bank -> Jolts
-large1 n = output . go n
-    where
-        go :: Int -> [Bat] -> [Bat]
-        go 0 _  = []
-        go k bs = max : go (k-1) rest
-            where
-                max  = (maximum . takeSec k) bs
-                rest = tail $ dropWhile (/= max) bs
-
-large2 :: Int -> Bank -> Jolts
-large2 n = output . go n
-    where
-        go :: Int -> [Bat] -> [Bat]
-        go 0 _  = []
-        go k bs = max : go (n-1) (tail $ dropWhile (/= max) bs)
-            where max = (maximum . takeSec n) bs
-
-large3 :: Int -> Bank -> Jolts
-large3 n = output . take n . go n
-    where go n bs = max : go (n-1) (tail $ dropWhile (/= max) bs)
-            where max = (maximum . takeSec n) bs
------------------------------------------------------------------------------
-
 largest' :: Int -> Bank -> Jolts
-largest' = large3
+largest' n = output . go n
+  where
+    go :: Int -> [Bat] -> [Bat]
+    go 0 _  = []
+    go k bs = max : go (k-1) rest
+      where
+        max  = (maximum . takeSec k) bs
+        rest = tail $ dropWhile (/= max) bs
 
 solve2 :: [Bank] -> Jolts
 solve2 = sum . map (largest' 12)
+
+-----------------------------------------------------
+
+day3 :: IO Int
+day3 = solve1 <$> day3Input
+
+day3p2 :: IO Int
+day3p2 = solve2 <$> day3Input
