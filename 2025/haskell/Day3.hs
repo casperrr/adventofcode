87bd0406b1a008e0c06979ca17965ab1c0318dc7
casperrr <casper.sebastien@gmail.com>
2025-12-04 16:56:54 +0000

    added code from some repo for day3 to figure it out

--- 2025/haskell/Day3.hs
+++ 2025/haskell/Day3.hs
@@ -51,3 +51,46 @@ day3 = solve1 <$> day3Input
 
 day3' :: IO Int
 day3' = solve2 <$> day3Input
+
+--------------------------------------------------------------------------------------
+-- I stole this from [here](https://github.com/RossPaterson/advent-of-code/blob/main/2025/Day03.hs) just so i could figure out how it works NOT MINE! but genius
+--------------------------------------------------------------------------------------
+
+solve2' :: [[Int]] -> Int
+solve2' = sum . map (maxSubNum 12)
+
+maxSubNum :: Int -> [Int] -> Int
+maxSubNum n bs = maxSubNums bs !! (n-1)
+
+maxSubNums :: [Int] -> [Int]
+maxSubNums = foldr addDigit []
+
+addDigit :: Int -> [Int] -> [Int]
+addDigit d best = zipWith max (best++[0]) (zipWith (+) (iterate (*10) d) (0:best))
+
+-- maxSubNums [4,9,8,1]
+-- foldr addDigit [] [4,9,8,1]
+-- addDigit 4 $ addDigit 9 $ addDigit 8 $ addDigit 1 []
+-- addDigit 4 $ addDigit 9 $ addDigit 8 $ zipWith max ([]++[0]) (zipWith (+) (iterate (*10) 1) (0:[]))
+-- zipWith max ([0]) (zipWith (+) (iterate (*10) 1) ([0]))
+-- zipWith max ([0]) (zipWith (+) [1,10,100,...] [0])
+-- zipWith max [0] [1]
+-- [1]
+-- addDigit 4 $ addDigit 9 $ addDigit 8 [1]
+-- addDigit 4 $ addDigit 9 $ zipWith max ([1]++[0]) (zipWith (+) (iterate (*10) 8) (0:[1]))
+-- zipWith max ([1]++[0]) (zipWith (+) (iterate (*10) 8) (0:[1]))
+-- zipWith max [1,0] (zipWith (+) [8,80,800,...] [0,1])
+-- zipWith max [1,0] [8,81]
+-- [8,81]
+-- addDigit 4 $ addDigit 9 [8,81]
+-- addDigit 4 $ zipWith max ([8,81]++[0]) (zipWith (+) (iterate (*10) 9) (0:[8,81]))
+-- zipWith max [8,81,0] (zipWith (+) [9,90,900,9000,...] [0,8,81])
+-- zipWith max [8,81,0] [9,98,981]
+-- [9,98,981]
+-- addDigit 4 [9,98,981]
+-- zipWith max ([9,98,981]++[0]) (zipWith (+) (iterate (*10) 4) (0:[9,98,981]))
+-- zipWith max ([9,98,981,0]) (zipWith (+) [4,40,400,4000,...] [0,9,98,981])
+-- zipWith max [9,98,981,0] [4,49,498,4981]
+-- [9,98,981,4981]
+
+-- damn this shit so clever
