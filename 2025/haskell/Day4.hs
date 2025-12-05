a6b798310bd3f3e90440ce9d8e7565563b6f758e
casperrr <casper.sebastien@gmail.com>
2025-12-05 20:09:57 +0000

    i think ive done it but ima need to clean it up

--- 2025/haskell/Day4.hs
+++ 2025/haskell/Day4.hs
@@ -2,7 +2,7 @@
 module Day4 where
 
 import Util.FetchInput ( fetchBodyStr, inputAOCURL )
-import Data.Set (Set)
+import Data.Set (Set, (\\))
 import qualified Data.Set as S
 
 type Pos = (Int, Int)
@@ -19,13 +19,15 @@ e1' = "..@@.@@@@.\n\
       \.@@@@@@@@.\n\
       \@.@.@@@.@."
 
-e1 :: Set Pos
+e1 :: [Pos]
 e1 = parse e1'
 
-parse :: String -> Set Pos
-parse s = S.fromList . map fst . filter ((== '@') . snd) $ [((i,j),x) | (i,row) <- zip [0..] (lines s), (j,x) <- zip [0..] row]
+-- parse :: String -> Set Pos
+-- parse s = S.fromList . map fst . filter ((== '@') . snd) $ [((i,j),x) | (i,row) <- zip [0..] (lines s), (j,x) <- zip [0..] row]
+parse :: String -> [Pos]
+parse s = map fst . filter ((== '@') . snd) $ [((i,j),x) | (i,row) <- zip [0..] (lines s), (j,x) <- zip [0..] row]
 
-day4Input :: IO (Set Pos)
+day4Input :: IO [Pos]
 day4Input = parse <$> fetchBodyStr (inputAOCURL 4)
 
 kernel :: Int -> Pos -> [Pos]
@@ -35,16 +37,60 @@ kernel k (x,y) = [(x', y') |
                    x' /= x || y' /= y]
 
 adjacent :: Pos -> Set Pos
-adjacent = S.fromList <$> kernel 1
+adjacent = S.fromList . kernel 1
 
-fuck :: Set Pos -> [Set Pos]
-fuck = map adjacent . S.toList
+adjRolls :: [Pos] -> [Set Pos]
+adjRolls ps = map (S.intersection (S.fromList ps) . adjacent) ps
 
-filtRolls :: Set Pos -> [Set Pos]
-filtRolls cs = map (S.intersection cs) (fuck cs)
+-- adjacent :: [Pos] -> [Set Pos]
+-- adjacent = map (S.fromList <$> kernel 1)
+
+-- filtRolls :: [Pos] -> [Set Pos]
+-- filtRolls cs = map (S.intersection $ S.fromList cs) (adjacent cs)
+
+findAcc :: [Set Pos] -> [Set Pos]
+findAcc = filter (\x -> S.size x < 4)
 
 count :: [Set Pos] -> Int
-count = length . filter (<4) . map S.size
+count = length . findAcc
 
-solve1 :: Set Pos -> Int
-solve1 = count . filtRolls
+solve1 :: [Pos] -> Int
+solve1 = count . adjRolls
+
+-----------------------------------------------------------------
+-- nah so whos a cunt then
+-----------------------------------------------------------------
+
+parse' :: String -> Set Pos
+parse' = S.fromList . parse
+
+e2 :: Set Pos
+e2 = parse' e1'
+
+adj :: Pos -> (Pos, Set Pos)
+adj p = (p, adjacent p)
+
+adjR :: Set Pos -> Set (Pos, Set Pos)
+adjR ps = S.map (\p -> let (_,x) = adj p in (p, S.intersection x ps)) ps
+
+acc :: Set (Pos, Set Pos) -> Set (Pos, Set Pos)
+acc = S.filter (\(_,x) -> S.size x < 4)
+
+solve1' :: Set Pos -> Int
+solve1' = S.size . acc . adjR
+
+-----------------------------------------------------------------
+-- Part 2 - Removing rolls
+-----------------------------------------------------------------
+
+removed :: Set Pos -> Set Pos
+removed = S.map fst . acc . adjR
+
+removeAll :: Set Pos -> [Int]
+removeAll ps
+    | S.null ps' = []
+    | otherwise = length ps' : removeAll (ps \\ ps')
+    where ps' = removed ps
+
+solve2' :: Set Pos -> Int
+solve2' = sum . removeAll
